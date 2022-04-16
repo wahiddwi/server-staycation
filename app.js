b0156a1eebce5afb067ecf33371abcd5b2d62381
wahiddwi <wahiddwi7@gmail.com>
2022-04-16 22:07:58 +0700

    connection mongoose

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//import mongoose
+const mongoose = require('mongoose');
+//connection mongoose
+mongoose.connect('mongodb://localhost:27017/myapp');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 //calling router admin
