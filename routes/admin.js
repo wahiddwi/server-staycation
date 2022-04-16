4f118cd6e953f442029d636d64ee913089cdca7e
wahiddwi <wahiddwi7@gmail.com>
2022-04-16 22:08:12 +0700

    routes

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,12 @@ const router = require('express').Router();
 //export controller 
 const adminController = require('../controllers/adminController');
 
+//route
 router.get('/dashboard', adminController.viewDashboard);
+router.get('/category', adminController.viewCategory);
+router.get('/bank', adminController.viewBank);
+router.get('/item', adminController.viewItem);
+router.get('/booking', adminController.viewBooking);
 
 //export router
 module.exports = router
