89ab972b502290cedf978e7893c429bbdf100422
wahiddwi <wahiddwi7@gmail.com>
2022-04-17 19:04:56 +0700

    master data

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//declare methodOverride
+const methodOverride = require('method-override');
 
 //import mongoose
 const mongoose = require('mongoose');
 //connection mongoose
-mongoose.connect('mongodb://localhost:27017/myapp');
+mongoose.connect('mongodb://localhost:27017/db_bwamern');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,7 +21,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+//use methodOverride
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,11 +1,49 @@
+//import model category
+const Category = require('../models/Category');
+
+
 //exports modul
 module.exports = {
     viewDashboard: (req, res) => {
         res.render('admin/dashboard/view_dashboard');
     },
 
-    viewCategory: (req, res) => {
-        res.render('admin/category/view_category');
+    viewCategory: async (req, res) => {
+        const category = await Category.find();
+        // console.log(category);
+        res.render('admin/category/view_category', { category });
+    },
+    addCategory: async (req, res) => {
+        //validation try catch
+        try {
+            const { name } = req.body;
+            // console.log(name);
+            await Category.create({ name });
+            res.redirect('/admin/category');
+        } catch (error) {
+            res.redirect('/admin/category');
+        }
+    },
+
+    editCategory: async (req, res) => {
+        //validation try catch
+        try {
+            const { id, name } = req.body;
+            const category = await Category.findOne({ _id: id });
+            // console.log(category);
+            category.name = name;
+            await category.save();
+            res.redirect('/admin/category');
+        } catch (error) {
+            res.redirect('/admin/category');
+        }
+    },
+
+    deleteCategory: async (req, res) => {
+        const { id } = req.params;
+        const category = await Category.findOne({ _id: id });
+        await category.remove();
+        res.redirect('/admin/category');
     },
 
     viewBank: (req, res) => {
