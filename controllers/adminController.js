2ae5781734c6eab346cfeb5ce35ed0554d94a2f0
wahiddwi <wahiddwi7@gmail.com>
2022-04-21 21:45:51 +0700

    update and delete master data bank

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,8 @@
 //import model
 const Category = require('../models/Category');
 const Bank = require('../models/Bank');
+const fs = require('fs-extra');
+const path = require('path');
 
 
 //exports modul
@@ -102,6 +104,57 @@ module.exports = {
                 imageUrl : `images/${req.file.filename}`
             });
             req.flash('alertMessage', 'Success Add Bank');
+            req.flash('alertStatus', 'success');
+            res.redirect('/admin/bank');
+        } catch (error) {
+            req.flash('alertMessage', `${error.message}`);
+            req.flash('alertStatus', 'danger');
+            res.redirect('/admin/bank');
+        }
+    },
+
+    editBank: async (req, res) => {
+        try {
+            const { id, name, nameBank, nomorRekening } = req.body;
+            //using function findOne for update data
+            const bank = await Bank.findOne({ _id: id });
+            //validasi
+            //jika tidak pilih gambar
+            if (req.file == undefined) {
+                bank.name = name;
+                bank.nameBank = nameBank;
+                bank.nomorRekening = nomorRekening;
+                await bank.save();
+                req.flash('alertMessage', 'Success Update Bank');
+                req.flash('alertStatus', 'success');
+                res.redirect('/admin/bank');
+            //jika gambar dipilih
+            } else {
+                //hapus gambar yang lama
+                await fs.unlink(path.join(`public/${bank.imageUrl}`));
+                bank.name = name;
+                bank.nameBank = nameBank;
+                bank.nomorRekening = nomorRekening;
+                bank.imageUrl = `images/${req.file.filename}`
+                await bank.save();
+                req.flash('alertMessage', 'Success Update Bank');
+                req.flash('alertStatus', 'success');
+                res.redirect('/admin/bank');
+            }
+        } catch (error) {
+            req.flash('alertMessage', `${error.message}`);
+            req.flash('alertStatus', 'danger');
+            res.redirect('/admin/bank');
+        }
+    },
+
+    deleteBank: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const bank = await Bank.findOne({ _id: id });
+            await fs.unlink(path.join(`public/${bank.imageUrl}`));
+            await bank.remove();
+            req.flash('alertMessage', 'Success Delete Bank');
             req.flash('alertStatus', 'success');
             res.redirect('/admin/bank');
         } catch (error) {
